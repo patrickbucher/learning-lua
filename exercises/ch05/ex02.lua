0e03c3f92954af6bb8576f3c5b6cc1a9b7129c4d
Patrick Bucher <patrick.bucher@mailbox.org>
2025-03-01 21:25:26 +0100

    chapter 5: exercise 2

--- exercises/ch05/ex02.lua
+++ exercises/ch05/ex02.lua
@@ -3,3 +3,7 @@ a = {}; a.a = a
 print(a.a.a.a) --> table
 -- the first a is a global variable
 -- the second a and the following as are indices
+
+a.a.a.a = 3
+print(a.a.a.a)
+-- now an error is thrown, because the reference to the table is no longer a table
