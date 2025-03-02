4a4dfa26fcc07194e8767dc44b69291c588c07b1
Patrick Bucher <patrick.bucher@mailbox.org>
2025-03-02 10:08:10 +0100

    rework of rounding functions

--- examples/round.lua
+++ examples/round.lua
@@ -1,8 +1,25 @@
-function round(x)
-    local f = math.floor(x)
-    if (x == f) or (x % 2.0 == 0.5) then
-        return f
+local round = function(x)
+    local lower = math.floor(x)
+    local upper = math.ceil(x)
+    if math.abs(x - lower) < math.abs(x - upper) then
+        return lower
     else
-        return math.floor(x + 0.5)
+        return upper
     end
+end
+
+local round_to = function(x, granularity)
+    local factor = 1.0 / granularity
+    return round((x * factor)) / factor
+end
+
+local numbers = { 0.499, 0.5, 0.501, 1 / 2, 10 / 3 }
+for i = 1, #numbers do
+    local n = numbers[i]
+    print(round(n))
+end
+
+for i = 1, #numbers do
+    local n = numbers[i]
+    print(round_to(n, 0.1))
 end
