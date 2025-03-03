679f65724b6b9044505496580582007e0ed125cb
Patrick Bucher <patrick.bucher@mailbox.org>
2025-03-03 10:48:12 +0100

    fixed nested lists

--- pandoc/bbcode.lua
+++ pandoc/bbcode.lua
@@ -3,17 +3,15 @@ function processListPlain(p)
 end
 
 function processBulletList(bl)
-    inner = bl:walk {
-        BulletList = function(bl) processBulletList(bl.content) end,
-        Plain = processListPlain,
+    local inner = bl:walk {
+        BulletList = function(l) processBulletList(l.content) end,
     }
     return string.format('[list]\n%s\n[/list]', pandoc.utils.stringify(inner))
 end
 
 function processOrderedList(bl)
-    inner = bl:walk {
-        BulletList = function(bl) processOrderedList(bl.content) end,
-        Plain = processListPlain,
+    local inner = bl:walk {
+        BulletList = function(l) processOrderedList(l.content) end,
     }
     return string.format('[list=1]\n%s\n[/list]', pandoc.utils.stringify(inner))
 end
@@ -23,8 +21,8 @@ filters = {
         local headerSizes = {
             [1] = 150, [2] = 130, [3] = 120, [4] = 115, [5] = 110, [6] = 105,
         }
-        size = headerSizes[h.level]
-        content = pandoc.utils.stringify(h.content)
+        local size = headerSizes[h.level]
+        local content = pandoc.utils.stringify(h.content)
         return string.format('[size=%d][b]%s[/b][/size]', size, content)
     end,
     Strong = function(s)
@@ -37,23 +35,23 @@ filters = {
         return string.format('[tt]%s[/tt]', pandoc.utils.stringify(c))
     end,
     BlockQuote = function(bq)
-        str = pandoc.utils.stringify(bq)
+        local str = pandoc.utils.stringify(bq)
         return pandoc.RawBlock('plain', string.format('[quote]\n%s\n[/quote]', str))
     end,
     CodeBlock = function(cb)
         return pandoc.RawBlock('plain', string.format('[code]\n%s\n[/%s]', cb.text, 'code'))
     end,
     Link = function(l)
-        content = pandoc.utils.stringify(l.content)
+        local content = pandoc.utils.stringify(l.content)
         return string.format('[url=%s]%s[/url]', l.target, content)
     end,
+    Plain = processListPlain,
     BulletList = processBulletList,
     OrderedList = processOrderedList,
 }
 
-function Writer(doc, opts)
+function Writer(doc, _opts)
     return pandoc.write(doc:walk(filters), 'plain', { wrap_text = 'wrap-none' })
 end
 
--- pandoc -t bbcode.lua input.md -o output.txt
--- FIXME: nested list actually starts as a list item of its own
+-- usage: pandoc -t bbcode.lua input.md -o output.txt
