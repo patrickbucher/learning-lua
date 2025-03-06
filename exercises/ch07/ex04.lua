99ac7dea77ec8d08c841e7f07d19037846dd0a78
Patrick Bucher <patrick.bucher@mailbox.org>
2025-03-06 17:23:06 +0100

    chapter 7: exercise 4

--- exercises/ch07/ex04.lua
+++ exercises/ch07/ex04.lua
@@ -1,22 +1,28 @@
 local find_last_newline = function(f)
-    local step = 80
     local size = f:seek("end")
-    local to = size
-    local from = size - 80
-    if from < 0 then
-        from = 0
-    end
-    while to > 0 do
-        f:seek("set", from)
-        local chunk = to - from + 1
-        local s = f:read(chunk)
+    local pos = size - 1
+    while pos > 0 do
+        f:seek("set", pos)
+        local s = f:read(1)
+        if s == '\n' and pos ~= size - 1 then
+            return pos
+        else
+            pos = pos - 1
+        end
     end
 end
 
 local get_last_line = function(file)
     local f, err = io.open(file, "r")
-    if err ~= nil then
+    if err ~= nil or f == nil then
         return nil
     end
     local last_newline_at = find_last_newline(f)
+    f:seek("set", last_newline_at + 1)
+    local last_line = f:read("l")
+    return last_line
 end
+
+local file = arg[1]
+local last_line = get_last_line(file)
+print(last_line)
